095cf4b368fada03ef933aef82a5dee8712b939a
Pablo Jarrin <pjarrin@sigma-alimentos.com>
2022-06-09 19:47:36 -0500

    Rutas hijas y LazyLoads

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { ErrorPageComponent } from './shared/error-page/error-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ErrorPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,27 @@
+import { NgModule, Component } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+
+
+const routes: Routes =[
+  {
+    path: '', children:[
+    {path: 'login', component: LoginComponent},
+    {path: 'registro', component: RegisterComponent},
+    {path: '**', redirectTo: 'login'},
+    ]
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(routes)
+
+  ],
+  exports:[
+    RouterModule
+
+  ]
+})
+export class AuthRoutingModule { }
